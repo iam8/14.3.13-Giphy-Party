9b3dca33780dbdd6dc250a6e5599177fad260d99
alexm <alex-m@outlook.com>
2022-11-17 15:47:54 -0800

    Renamed async function; added parameter for search term for the searchForGif() function; added code to empty the search box value after submission

--- app.js
+++ app.js
@@ -5,18 +5,22 @@
 console.log("Let's get this party started!");
 
 const $form = $("#search-form");
+const $searchInput = $("#search-input");
 
 // Add event listener for submitting the form
 $form.on("submit", function(evt) {
     evt.preventDefault();
-    req();
+
+    searchForGif($searchInput.val());
+    $searchInput.val("");
+
 })
 
-async function req() {
+async function searchForGif(searchTerm) {
     const getRes = await axios.get("https://api.giphy.com/v1/gifs/search", {
         params: {
             api_key: "g34JBy4NPfyJgqBiQ4LwU0bQFXySDlg0",
-            q: "koala"
+            q: searchTerm
         }});
 
         console.log(getRes.data.data[0]);
