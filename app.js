f7d6fcfb593f07260594a32a82da44291b70694d
alexm <alex-m@outlook.com>
2022-11-17 15:31:29 -0800

    Added event listener for form submission (sends GET request via Axios with hardcoded params)

--- app.js
+++ app.js
@@ -3,3 +3,19 @@
 // 14.3.13: Giphy Party Exercise
 
 console.log("Let's get this party started!");
+
+const $form = $("#search-form");
+
+// Add event listener for submitting the form
+$form.on("submit", function(evt) {
+    evt.preventDefault();
+    req();
+})
+
+async function req() {
+    const getRes = await axios.get("https://api.giphy.com/v1/gifs/search", {
+        params: {
+            api_key: "g34JBy4NPfyJgqBiQ4LwU0bQFXySDlg0",
+            q: "koala"
+        }});
+}
